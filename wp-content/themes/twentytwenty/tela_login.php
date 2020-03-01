f1f5321755aeae307bf1c136d2dae2382cc78219
hugo <hugo_3wl@yahoo.com.br>
2020-03-01 15:47:56 -0300

    incluindo cabecalho personalizado

--- wp-content/themes/twentytwenty/tela_login.php
+++ wp-content/themes/twentytwenty/tela_login.php
@@ -134,7 +134,6 @@ $username_err = $password_err = "";
         .wrapper{ width: 350px; padding: 20px; }
     </style>
 
-<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
 		<link rel="profile" href="https://gmpg.org/xfn/11">
