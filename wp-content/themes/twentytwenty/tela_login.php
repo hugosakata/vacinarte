4b375443f7101e13b59de28a4f36804251c4d1de
hugo <hugo_3wl@yahoo.com.br>
2020-03-01 15:16:38 -0300

    teste redirecionamento

--- wp-content/themes/twentytwenty/tela_login.php
+++ wp-content/themes/twentytwenty/tela_login.php
@@ -33,7 +33,8 @@ $username_err = $password_err = "";
             foreach ( $pages as $page ) {
                 $msg_err .= get_page_link( $page->ID ) . ",\n";
             }
-            exit( wp_redirect( "http://vacinarte-admin.com.br/teste/" ) );
+            //exit( wp_redirect( "http://vacinarte-admin.com.br/teste/" ) );
+            window.location.href="http://vacinarte-admin.com.br/teste/";
         } else {
             $msg_err="Não achou!";
         }
