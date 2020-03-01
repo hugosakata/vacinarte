e229bbd0dbed2a66b092ae65c4f209d0d32482e1
hugo <hugo_3wl@yahoo.com.br>
2020-03-01 12:36:25 -0300

    teste 1

--- wp-content/themes/twentytwenty/custompage1.php
+++ wp-content/themes/twentytwenty/custompage1.php
@@ -10,91 +10,93 @@ get_header(); ?>
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
  
+$username_err = "teste";
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+// if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-    // Validate username
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT id FROM users WHERE username = ?";
+//     // Validate username
+//     if(empty(trim($_POST["username"]))){
+//         $username_err = "Please enter a username.";
+//     } else{
+//         // Prepare a select statement
+//         $sql = "SELECT id FROM users WHERE username = ?";
         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
+//         if($stmt = mysqli_prepare($link, $sql)){
+//             // Bind variables to the prepared statement as parameters
+//             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
-            // Set parameters
-            $param_username = trim($_POST["username"]);
+//             // Set parameters
+//             $param_username = trim($_POST["username"]);
             
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result */
-                mysqli_stmt_store_result($stmt);
+//             // Attempt to execute the prepared statement
+//             if(mysqli_stmt_execute($stmt)){
+//                 /* store result */
+//                 mysqli_stmt_store_result($stmt);
                 
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
+//                 if(mysqli_stmt_num_rows($stmt) == 1){
+//                     $username_err = "This username is already taken.";
+//                 } else{
+//                     $username = trim($_POST["username"]);
+//                 }
+//             } else{
+//                 echo "Oops! Something went wrong. Please try again later.";
+//             }
 
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
+//             // Close statement
+//             mysqli_stmt_close($stmt);
+//         }
+//     }
     
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
+//     // Validate password
+//     if(empty(trim($_POST["password"]))){
+//         $password_err = "Please enter a password.";     
+//     } elseif(strlen(trim($_POST["password"])) < 6){
+//         $password_err = "Password must have atleast 6 characters.";
+//     } else{
+//         $password = trim($_POST["password"]);
+//     }
     
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
+//     // Validate confirm password
+//     if(empty(trim($_POST["confirm_password"]))){
+//         $confirm_password_err = "Please confirm password.";     
+//     } else{
+//         $confirm_password = trim($_POST["confirm_password"]);
+//         if(empty($password_err) && ($password != $confirm_password)){
+//             $confirm_password_err = "Password did not match.";
+//         }
+//     }
     
-    // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+//     // Check input errors before inserting in database
+//     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
         
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+//         // Prepare an insert statement
+//         $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
          
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+//         if($stmt = mysqli_prepare($link, $sql)){
+//             // Bind variables to the prepared statement as parameters
+//             mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
             
-            // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+//             // Set parameters
+//             $param_username = $username;
+//             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
             
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Redirect to login page
-                header("location: login.php");
-            } else{
-                echo "Something went wrong. Please try again later.";
-            }
+//             // Attempt to execute the prepared statement
+//             if(mysqli_stmt_execute($stmt)){
+//                 // Redirect to login page
+//                 header("location: login.php");
+//             } else{
+//                 echo "Something went wrong. Please try again later.";
+//             }
 
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
+//             // Close statement
+//             mysqli_stmt_close($stmt);
+//         }
+//     }
     
-    // Close connection
-    mysqli_close($link);
-}
+//     // Close connection
+//     mysqli_close($link);
+// }
 ?>
  
 <!DOCTYPE html>
@@ -116,14 +118,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 		    <?php get_template_part( 'template-parts/page/content', 'page' );?>
     
     <center><div class="wrapper">
+
+
     <?php
-global $wpdb;
+    global $wpdb;
 
-$user_count = $wpdb->get_row( "SELECT ifnull(teste1, 0) as ativo, count(teste1) as existe FROM TESTE where teste1=2 and teste3=2" );
-echo "<p>ativo: {$user_count->ativo} existe {$user_count->existe}</p>";
+    $user_count = $wpdb->get_row( "SELECT ifnull(teste1, 0) as ativo, count(teste1) as existe FROM TESTE where teste1=2 and teste3=2" );
+    echo "<p>ativo: {$user_count->ativo} existe {$user_count->existe}</p>";
 
-?>
-        <h2>-----------------Sign Up-------------</h2>
+    ?>
+
+    
+        <h2>Sign Up</h2>
         <p>Please fill this form to create an account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
