9f42bcda56690f5e8c7443cedd8d447d85af0a06
hugo <hugo_3wl@yahoo.com.br>
2020-03-01 11:56:46 -0300

    start project

---
+++ public_html/wp-config.php
@@ -0,0 +1,82 @@
+<?php
+define('WP_CACHE', true);
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+//define( 'DB_NAME', 'u414589721_0BIRA' );
+define( 'DB_NAME', 'u414589721_vacinarte' );
+
+/** MySQL database username */
+//define( 'DB_USER', 'u414589721_Pqmkc' );
+define( 'DB_USER', 'u414589721_vacinarte' );
+
+/** MySQL database password */
+//define( 'DB_PASSWORD', '1ADFHgUesf' );
+define( 'DB_PASSWORD', 'vacinarte' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'mysql' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',          'D(4CN94ZY&$uGNa!}]YD,&}Vcf=bqmTL)Po&:%Az4scNzmsp!X/G.V=vcj~m_6)M' );
+define( 'SECURE_AUTH_KEY',   '2;pp,7b+--U?Hm4T0|*3qD+L)0ojBOV4&V!7,G$hm&BAjgJ.{j8dG)vwO)3nI+vL' );
+define( 'LOGGED_IN_KEY',     'dE}zT&WL,8%b(R15j~f6};C9LB)_8`!wp4%VSOipkM ZlT39oAtZ+$h,|wA:[pBX' );
+define( 'NONCE_KEY',         ',EKPrU8hl8R&EUmrOtddH]osg9Q<uv)z)qxi4CKpjz~O.h9a m3PZymMr.*U.=AA' );
+define( 'AUTH_SALT',         '5YtuNa]%79ZjATmm4Kw-_8)6C7Jhv8mK^O7uNo>l Sz*IP)t#C@,[+woiwOKCXYU' );
+define( 'SECURE_AUTH_SALT',  'uO+3BK [ He(Jl(T()BK[W5jQ9LmxL5 Yh94@:f9UkYB.%B!1hh:3Vd}na1&4_4;' );
+define( 'LOGGED_IN_SALT',    'mzx-5k:vg#a~*8CeeN7PqDx:m:;-%q!T/j?Zrbz*ZNbK/O.UaCqx2V:dSdBx>+X0' );
+define( 'NONCE_SALT',        'O`uGvY0E=d/.7)]c{y]7[t#wQ,.=.vJwi]h;61w*C#{egrc?9qwQ.-<;Y?,!fPl4' );
+define( 'WP_CACHE_KEY_SALT', '+&d{uU)kYbTR@W${He_buMu7eGb,P(/L:T:yuJ}$<EE{E)qCzui@W[Pj!RnbP :M' );
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ public_html/zyro/1.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Home</title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=1200" />
+		<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<!-- Facebook Open Graph -->
+	<meta property="og:title" content="Home" />
+	<meta property="og:description" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content="{{curr_url}}" />
+	<!-- Facebook Open Graph end -->
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=20190120113454" type="text/javascript"></script>
+
+	<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
+	<link href="css/site.css?v=20190120113454" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1583009795" rel="stylesheet" type="text/css" />
+	<link href="css/1.css?ts=1583009795" rel="stylesheet" type="text/css" />
+	<ga-code/>
+	<script type="text/javascript">
+	window.useTrailingSlashes = true;
+</script>
+	
+	<link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />	
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<![endif]-->
+
+	</head>
+
+
+<body><div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div class="wb_cont_inner"></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div class="wb_cont_inner"><div id="wb_element_instance1" class="wb_element wb_text_element" style=" line-height: normal;"><h4 class="wb-stl-pagetitle">Vacinarte</h4></div><div id="wb_element_instance2" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(1);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance2");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance2").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_footer">
+	
+<div class="wb_cont_inner" style="height: 280px;"><div id="wb_element_instance0" class="wb_element"><form class="wb_form" method="post" enctype="multipart/form-data"><input type="hidden" name="wb_form_id" value="48a8d4b5"><textarea name="message" rows="3" cols="20" class="hpc"></textarea><table><tr><th>Login&nbsp;&nbsp;</th><td><input type="hidden" name="wb_input_0" value="Login"><input class="form-control form-field" type="text" value="" name="wb_input_0" required="required"></td></tr><tr><th>Senha&nbsp;&nbsp;</th><td><input type="hidden" name="wb_input_1" value="Senha"><input class="form-control form-field" type="text" value="" name="wb_input_1" required="required"></td></tr><tr class="form-footer"><td colspan="2"><button type="submit" class="btn btn-default">Entrar</button></td></tr></table></form><script type="text/javascript">
+			<?php $wb_form_id = popSessionOrGlobalVar("wb_form_id"); if ($wb_form_id == "48a8d4b5") { ?>
+				var formValues = <?php echo json_encode(popSessionOrGlobalVar("post")); ?>;
+				var formErrors = <?php echo json_encode(popSessionOrGlobalVar("formErrors")); ?>;
+				wb_form_validateForm("48a8d4b5", formValues, formErrors);
+				<?php $wb_form_send_success = popSessionOrGlobalVar("wb_form_send_success");
+				if (($wb_form_send_state = popSessionOrGlobalVar("wb_form_send_state"))) { ?>
+					var prompt = $("<div>")
+						.addClass("alert alert-<?php echo $wb_form_send_success ? "success" : "danger"; ?>")
+						.css({ position: "fixed", opacity: 0, right: "-50px", top: "10px", zIndex: 10000, fontSize: "24px",
+							   padding: "30px 50px", lineHeight: "24px", maxWidth: "748px" })
+						.append("<?php echo str_replace('"', '\"', $wb_form_send_state); ?>")
+						.prepend($("<button>").addClass("close")
+							.css({ marginRight: "-40px", marginTop: "-24px" })
+							.html("&nbsp;&times;")
+							.on("click", function() { $(this).parent().remove(); })
+						)
+					.appendTo("body");
+					setTimeout(function() { prompt.animate({ opacity: 1, right: "10px" }, 250); }, 250);
+					<?php $wb_form_send_success = false; $wb_form_send_state = null; ?>
+				<?php } ?>
+			<?php } ?>
+			</script></div><div id="wb_element_instance3" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
+</html>
