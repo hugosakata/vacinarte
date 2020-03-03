f3dc9c640387459fea7170713b12a900183fa879
hugo <hugo_3wl@yahoo.com.br>
2020-03-03 20:37:57 -0300

    corrige link

--- wp-content/themes/twentytwenty/tela_homelogada.php
+++ wp-content/themes/twentytwenty/tela_homelogada.php
@@ -128,7 +128,7 @@ img.emoji {
                                         </div><!-- fecha col xm 12 -->
                                     </div><!-- fecha row -->
                                 </div><!-- fecha panel heading -->
-                                <a href="http://vacinarte-admin.com.br/ccadastrar-pj/">
+                                <a href="http://vacinarte-admin.com.br/cadastrar-pj/">
                                     <div class="panel-footer">
                                         <span class="pull-left">Listar clientes</span>
                                         <span class="pull-right"><i class="glyphicon glyphicon-circle-arrow-right"></i></span>
