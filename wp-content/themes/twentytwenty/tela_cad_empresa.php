7a3cc94778cd54b6bb2dd6d67664d127e357c127
Reinaldo Daltro <rei.dev.syst@gmail.com>
2020-03-11 21:57:00 -0300

    testando mesma url nos 2 btns

--- wp-content/themes/twentytwenty/tela_cad_empresa.php
+++ wp-content/themes/twentytwenty/tela_cad_empresa.php
@@ -123,7 +123,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
               value="Endereços" <?php if ($id_retorno <= 0) { echo "disabled='true' style='background-color:slateGray'"; } ?>/>
             </div> 
             <div class="col-xs-2 col-xs-offset-1">
-              <input type="button" onclick="<?php echo "location.href='http://vacinarte-admin.com.br/listar-contatos/?id='{$id_retorno}'"; ?>" 
+              <input type="button" onclick="<?php echo "location.href='http://vacinarte-admin.com.br/listar-enderecos/?id='{$id_retorno}'"; ?>" 
               value="Contatos" <?php if ($id_retorno <= 0) { echo "disabled='true' style='background-color:slateGray'"; } ?>/>
             </div> 
           </div>
