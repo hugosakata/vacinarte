b4cdc432755bdaba75ad9b818b1624e1b28b21da
ReiDaltro <rei.dev.syst@gmail.com>
2020-03-14 22:12:17 -0300

    testando link de bootstrap

--- wp-content/themes/twentytwenty/tela_lista_endereco.php
+++ wp-content/themes/twentytwenty/tela_lista_endereco.php
@@ -15,22 +15,18 @@ if(isset($_GET['id'])){
    
     <title>Endereços do Cliente</title>
     <!-- Bootstrap -->
-    <link href="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/css/bootstrap.min.css" rel="stylesheet"> -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <!-- Cesup Styles -->
     <link href="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/css/styles.css" rel="stylesheet" >
     <!-- Google Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!-- DataTable -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="../DataTables/css/dataTables.bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../DataTables/css/dataTables.jqueryui.min.css">
-    <link href="../css/datatables.min.css" rel="stylesheet" >
-    <script type="text/javascript" src="../DataTables/js/dataTables.bootstrap.min.js"></script>
-    <script type="text/javascript" src="../DataTables/js/dataTables.jqueryui.min.js"></script>
-    <script type="text/javascript" src="../DataTables/js/jquery.dataTables.min.js"></script> -->
-
-    <!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script> -->
+    
 
   </head>
   <body>
