ad77b757d6733ece45050f931ebd41f183c4aee9
ReiDaltro <rei.dev.syst@gmail.com>
2020-03-15 17:08:06 -0300

    alimentando select de empresa na tela cad campanha3

--- wp-content/themes/twentytwenty/tela_cad_campanha.php
+++ wp-content/themes/twentytwenty/tela_cad_campanha.php
@@ -143,7 +143,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 foreach ( $clientes as $cliente ) 
                 {
               ?>
-                <option value=""></option>
+                <!-- <option value=""></option> -->
                 <option value=<?php echo $cliente->cd_cli ?>;><?php echo $cliente->nm_fant ?></option>
               </select>
             <?php
