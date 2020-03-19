b430bd3dd295c838f60db74a013992a831de2652
hugo <hugo_3wl@yahoo.com.br>
2020-03-18 23:02:09 -0300

    tela cad agenda

--- wp-content/themes/twentytwenty/tela_cad_agenda.php
+++ wp-content/themes/twentytwenty/tela_cad_agenda.php
@@ -146,7 +146,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       <div class="accordion-group">
         <div class="accordion-heading">
           <a class="accordion-toggle" data-toggle="collapse"
-            data-parent="#searchAccordion" href="#collapseOne"><h3><small>Dados da campanha</small></h3></a>
+            data-parent="#searchAccordion" id="idOne"><h3><small>Dados da campanha</small></h3></a>
         </div>
         <div id="collapseOne" class="accordion-body collapse in">
           <div class="accordion-inner">
@@ -190,7 +190,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       <div class="accordion-group">
         <div class="accordion-heading">
           <a class="accordion-toggle" data-toggle="collapse"
-            data-parent="#searchAccordion" href="#collapseTwo"><h3>Dados do agendamento</h3></a>
+            data-parent="#searchAccordion" id="idTwo"><h3><small>Dados do agendamento</small></h3></a>
         </div>
         <div id="collapseTwo" class="accordion-body collapse">
           <div class="accordion-inner">
@@ -241,7 +241,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       </div><!-- fecha col 12 -->
     </div><!-- fecha row txtbox -->
 
-    
+    <script>
+    $("idOne").click(function(){
+      if (document.getElementById('collapseOne').classList.contains("accordion-body collapse in")){
+        document.getElementById('collapseOne').setAttribute('class','accordion-body collapse');
+      } else {
+        document.getElementById('collapseOne').setAttribute('class','accordion-body collapse in');
+      }
+    });
+    $("idTwo").click(function(){
+      if (document.getElementById('collapseTwo').classList.contains("accordion-body collapse in")){
+        document.getElementById('collapseTwo').setAttribute('class','accordion-body collapse');
+      } else {
+        document.getElementById('collapseTwo').setAttribute('class','accordion-body collapse in');
+      }
+      
+    });
+    </script>
 
 
     
