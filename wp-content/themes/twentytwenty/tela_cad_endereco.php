13d6bf2333ca9e0fb47fe03cd606c542263e3a21
hugo <hugo_3wl@yahoo.com.br>
2020-03-19 22:32:21 -0300

    manter na pagina a salvar endereco

--- wp-content/themes/twentytwenty/tela_cad_endereco.php
+++ wp-content/themes/twentytwenty/tela_cad_endereco.php
@@ -97,8 +97,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       echo "<script language='javascript' type='text/javascript'>
       alert('Endereço salvo com sucesso!');</script>";
 
-      echo "<script language='javascript' type='text/javascript'>
-        window.location.href='http://vacinarte-admin.com.br/listar-enderecos/?id={$id_cli}';</script>";
+      // echo "<script language='javascript' type='text/javascript'>
+      //   window.location.href='http://vacinarte-admin.com.br/listar-enderecos/?id={$id_cli}';</script>";
 
     } else {
       // $msg_err .= $wpdb->show_errors();
