bd61a68e91da580de872e646f3ab358f905deb81
ReiDaltro <rei.dev.syst@gmail.com>
2020-03-22 01:00:42 -0300

    testando menu direto na tela lista contato

--- wp-content/themes/twentytwenty/tela_lista_contato.php
+++ wp-content/themes/twentytwenty/tela_lista_contato.php
@@ -35,13 +35,37 @@ if(isset($_GET['id'])){
   </head>
   <style>
     .corpo{
-          background-color: WhiteSmoke;
-        }
+      background-color: WhiteSmoke;
+    }
+    .texto_cabeca{
+      font-size: 25px;
+      margin-top: 2vw !important;
+      color: dimgray;
+    }
+    .barra4vw{
+      height: 4vw !important;
+    }
+    .cabeca{
+      border: none;
+      margin-left: -15px;
+      width: 103%;
+    }
+    .link_home{
+      margin-left: 2vw;
+      text-decoration: none;
+      color: #353b48;
+      font-size: 18px;
+      font-weight: bold;
+    }
+    .fontMenu{
+      font-size: 15px;
+      font-weight: bold;
+    }
     #tab_lista_contatos_info, #tab_lista_contatos_paginate{
       font-size: 15px;
     }
     input{
-      height: 1vw;
+      height: 2vw;
     }
     #tab_lista_contatos_length{
       width: 30%;
@@ -54,100 +78,151 @@ if(isset($_GET['id'])){
     #tab_lista_contatos_filter.label#text{
       text-align: left;
     }
-    .btn_salvar{
-      margin-top: 2.6vw;
-      height: 4.5vw;
+    #btn_salvar{
+      width: 8vw;
+      font-size: 14px;
+      border-radius: 6px;
+      height: 3.5vw;
+      margin-top: 1vw;
     }
   </style>
   <body class="corpo">
   
-  <?php include 'tela_header.php';?>
-  <?php if ($_COOKIE["logado"] <= 0){
+    <?php if ($_COOKIE["logado"] <= 0){
         echo "<script language='javascript' type='text/javascript'>
         window.location.href='http://vacinarte-admin.com.br/';</script>";
     }?>
   
-<div class="container"><!-- container principal-->
-    
-    <div class="row">
-        <div class="col-xs-10">
-          <h3 class="page-header">Contatos do cliente
-          <!-- <br>
-            <small>Preencha o formulário abaixo para cadastrar um novo cliente</small> -->
-          </h3>
-        </div>
-        <div class="col-xs-2" style="align:center">
-          <input class="btn_salvar pull-right" type="button" onclick="location.href='http://vacinarte-admin.com.br/cadastrar-contato/?id=<?php echo $id_cli; ?>';" 
-          value="Novo" style="margin-top:35px"/>
-        </div>
-    </div><!-- fecha div row -->
-
-    <div class="row txtbox"><!-- row formulario -->
-      <div class="col-lg-12 col-xs-12">
-        <div class="row"><!--div row painel de consulta -->
-          <div class="tab-content">
-          <div role="tabpanel" class="tab-pane fade in active">
-            <div class="col-lg-12 col-xs-12">
-              <div class="row paineldeconsulta">
-                <div class="col-lg-12 col-xs-12"><!-- posiciona painel -->
-                  <div class="panel panel-default">
-                    <div class="panel-body">
-                      <table class="table table-striped" id="tab_lista_contatos">
-                        <thead>
-                          <tr>
-                            <th>Nome do contato </th>
-                            <th>Telefone</th>
-                            <th>Email</th>
-                            <th>Observação</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                        <?php
-                          $contatos = $wpdb->get_results( 
-                            "
-                            SELECT `cd_ctt`, `cd_cli`, `nm_ctt`, `tel_pri`, 
-                            `tel_sec`, `email`, `linkedin`, `site_blog`, `obs_ctt` 
-                            FROM `CONTATO`
-                            WHERE cd_cli={$id_cli} order by `nm_ctt`
-                            "
-                          );
-                          
-                          if (count($contatos)<=0){
-                            echo "<script language='javascript' type='text/javascript'>
-                            window.location.href='http://vacinarte-admin.com.br/cadastrar-contato/?id={$id_cli}';</script>";
-                          }
-
-                          foreach ( $contatos as $contato ) 
-                          {
-                        ?>
-                          <tr>
-                            <td><?php echo $contato->nm_ctt ?></td>
-                            <td><?php echo $contato->tel_pri ?></td>
-                            <td><?php echo $contato->email ?></td>
-                            <td><?php echo $contato->obs_ctt ?></td>
-                            <!-- <td>
-                              <a><i class="material-icons" style="padding-left: 5px; color: CornflowerBlue; cursor: pointer;">description</i></a>
-                              <a><i class="material-icons" style="padding-left: 5px; color: SlateGray; cursor: pointer;">edit</i></a>
-                              <a><i class="material-icons" style="padding-left: 5px; color: tomato; cursor: pointer;">delete</i></a>
-                            </td> -->
-                          </tr>
-                          <?php
-                            }
-                          ?>
+  <div class="container-fluid barra4vw">
+    <!-- <span><a class="" data-toggle="modal" data-target="#modalBtnCad">Cadastrar</a></span> -->
+    <div >
+        <nav class="navbar navbar-default cabeca barra4vw">
+          <div class="container-fluid barra4vw" style="background-color: Gainsboro;">
+            <!-- Brand and toggle get grouped for better mobile display -->
         
-                        </tbody>
-                      </table>
-                    </div><!-- fecha panel corpo -->
-                  </div><!-- fecha panel default -->
-                </div><!-- fecha col lg 12 -->
-              </div><!-- fecha row paineldemequipe -->
-          </div><!-- fecha col lg 12 -->
-          </div><!-- fecha div painel pdequipe -->
-        </div>
-        </div>
-      </div><!-- fecha col 12 -->
-    </div><!-- fecha row txtbox -->
-</div><!-- fecha container principal -->  
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="navbar barra4vw">
+              <ul class="nav navbar-nav" style="margin-top: 1vw;">
+                <a class="link_home" href="http://vacinarte-admin.com.br/home"><span>Vacinarte</span></a>
+              </ul>
+
+              <ul class="nav navbar-nav" style="margin-left: 48vw;">
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle fontMenu" data-toggle="dropdown" 
+                    role="button" aria-haspopup="true" 
+                    aria-expanded="false">Cadastrar <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <!-- <li><a href="http://vacinarte-admin.com.br/cadastrar-pf/">Pessoa física</a></li> -->
+                    <li><a href="http://vacinarte-admin.com.br/cadastrar-pj/">Pessoa jurídica</a></li>
+                    <li><a href="http://vacinarte-admin.com.br/campanha/">Campanha</a></li>
+                  </ul>
+                </li>
+              </ul>
+              
+              <ul class="nav navbar-nav">
+                <li class="dropdown">
+                  <a href="#" style="text-decoration: none;" class="dropdown-toggle fontMenu" 
+                    data-toggle="dropdown" role="button" aria-haspopup="true" 
+                    aria-expanded="false">Listar <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <!-- <li><a href="http://vacinarte-admin.com.br/listar-pf/">Clientes PF</a></li> -->
+                    <li><a href="http://vacinarte-admin.com.br/listar-pj/">Pessoa jurídica</a></li>
+                    <li><a href="http://vacinarte-admin.com.br/listar-campanhas/">Campanhas</a></li>
+                  </ul>
+                </li>
+              </ul>
+
+              <ul class="nav navbar-nav">
+                <!-- <li><a style="text-decoration: none;" href="#" data-toggle="modal" data-target="#modalBtnCad">Cadastrar</a></li> -->
+                <li><a style="text-decoration: none;" class="fontMenu" href="http://vacinarte-admin.com.br/listar-agendamento/">Agenda</a></li>
+                <li><a style="text-decoration: none;" class="fontMenu" href="https://www.vacinarte.com.br/">Site Vacinarte</a></li>
+                <li class="page_item page-item-13 fontMenu"><a style="text-decoration: none;" href="http://vacinarte-admin.com.br/?sair=true">Sair</a></li>
+              </ul>            
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+      </div>
+
+    </div>
+  
+    <div class="container"><!-- container principal-->
+      
+      <div class="row">
+          <div class="col-xs-10">
+            <h3 class="page-header texto_cabeca">Contatos do cliente</h3>
+          </div>
+          <div class="col-xs-2" style="align:center">
+            <input id="btn_salvar" class="btn btn-danger pull-right" type="button" onclick="location.href='http://vacinarte-admin.com.br/cadastrar-contato/?id=<?php echo $id_cli; ?>';" 
+            value="Novo" />
+          </div>
+      </div><!-- fecha div row -->
+
+      <div class="row txtbox"><!-- row formulario -->
+        <div class="col-lg-12 col-xs-12">
+          <div class="row"><!--div row painel de consulta -->
+            <div class="tab-content">
+            <div role="tabpanel" class="tab-pane fade in active">
+              <div class="col-lg-12 col-xs-12">
+                <div class="row paineldeconsulta">
+                  <div class="col-lg-12 col-xs-12"><!-- posiciona painel -->
+                    <div class="panel panel-default">
+                      <div class="panel-body">
+                        <table class="table table-striped" id="tab_lista_contatos">
+                          <thead>
+                            <tr>
+                              <th>Nome do contato </th>
+                              <th>Telefone</th>
+                              <th>Email</th>
+                              <th>Observação</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                          <?php
+                            $contatos = $wpdb->get_results( 
+                              "
+                              SELECT `cd_ctt`, `cd_cli`, `nm_ctt`, `tel_pri`, 
+                              `tel_sec`, `email`, `linkedin`, `site_blog`, `obs_ctt` 
+                              FROM `CONTATO`
+                              WHERE cd_cli={$id_cli} order by `nm_ctt`
+                              "
+                            );
+                            
+                            if (count($contatos)<=0){
+                              echo "<script language='javascript' type='text/javascript'>
+                              window.location.href='http://vacinarte-admin.com.br/cadastrar-contato/?id={$id_cli}';</script>";
+                            }
+
+                            foreach ( $contatos as $contato ) 
+                            {
+                          ?>
+                            <tr>
+                              <td><?php echo $contato->nm_ctt ?></td>
+                              <td><?php echo $contato->tel_pri ?></td>
+                              <td><?php echo $contato->email ?></td>
+                              <td><?php echo $contato->obs_ctt ?></td>
+                              <!-- <td>
+                                <a><i class="material-icons" style="padding-left: 5px; color: CornflowerBlue; cursor: pointer;">description</i></a>
+                                <a><i class="material-icons" style="padding-left: 5px; color: SlateGray; cursor: pointer;">edit</i></a>
+                                <a><i class="material-icons" style="padding-left: 5px; color: tomato; cursor: pointer;">delete</i></a>
+                              </td> -->
+                            </tr>
+                            <?php
+                              }
+                            ?>
+          
+                          </tbody>
+                        </table>
+                      </div><!-- fecha panel corpo -->
+                    </div><!-- fecha panel default -->
+                  </div><!-- fecha col lg 12 -->
+                </div><!-- fecha row paineldemequipe -->
+            </div><!-- fecha col lg 12 -->
+            </div><!-- fecha div painel pdequipe -->
+          </div>
+          </div>
+        </div><!-- fecha col 12 -->
+      </div><!-- fecha row txtbox -->
+    </div><!-- fecha container principal -->  
 
 <script src="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/jquery.min.js"></script>
     <script src="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/bootstrap.min.js"></script>
