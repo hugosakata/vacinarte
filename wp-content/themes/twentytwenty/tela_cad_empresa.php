bf9cd0c6f7afcb573dc2f7eaa407976b920b0f5c
ReiDaltro <rei.dev.syst@gmail.com>
2020-03-22 02:32:53 -0300

    testando menu direto na tela cad contato

--- wp-content/themes/twentytwenty/tela_cad_empresa.php
+++ wp-content/themes/twentytwenty/tela_cad_empresa.php
@@ -149,120 +149,116 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         window.location.href='http://vacinarte-admin.com.br/';</script>";
     }?>
 
-<div class="container-fluid barra4vw">
-    <!-- <span><a class="" data-toggle="modal" data-target="#modalBtnCad">Cadastrar</a></span> -->
-    <div >
-        <nav class="navbar navbar-default cabeca barra4vw">
-          <div class="container-fluid barra4vw" style="background-color: Gainsboro;">
-            <!-- Brand and toggle get grouped for better mobile display -->
-        
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="navbar barra4vw">
-              <ul class="nav navbar-nav" style="margin-top: 1vw; float: left;">
-                <a class="link_home" href="http://vacinarte-admin.com.br/home"><span>Vacinarte</span></a>
-              </ul>
+    <div class="container-fluid barra4vw">
+      <!-- <span><a class="" data-toggle="modal" data-target="#modalBtnCad">Cadastrar</a></span> -->
+      <div >
+          <nav class="navbar navbar-default cabeca barra4vw">
+            <div class="container-fluid barra4vw" style="background-color: Gainsboro;">
+              <!-- Brand and toggle get grouped for better mobile display -->
+          
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="navbar barra4vw">
+                <ul class="nav navbar-nav" style="margin-top: 1vw; float: left;">
+                  <a class="link_home" href="http://vacinarte-admin.com.br/home"><span>Vacinarte</span></a>
+                </ul>
 
-              <ul class="nav navbar-nav" style="margin-left: 48vw;">
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle fontMenu" data-toggle="dropdown" 
-                    role="button" aria-haspopup="true" 
-                    aria-expanded="false">Cadastrar <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <!-- <li><a href="http://vacinarte-admin.com.br/cadastrar-pf/">Pessoa física</a></li> -->
-                    <li><a href="http://vacinarte-admin.com.br/cadastrar-pj/">Pessoa jurídica</a></li>
-                    <li><a href="http://vacinarte-admin.com.br/campanha/">Campanha</a></li>
-                  </ul>
-                </li>
-              </ul>
-              
-              <ul class="nav navbar-nav">
-                <li class="dropdown">
-                  <a href="#" style="text-decoration: none;" class="dropdown-toggle fontMenu" 
-                    data-toggle="dropdown" role="button" aria-haspopup="true" 
-                    aria-expanded="false">Listar <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <!-- <li><a href="http://vacinarte-admin.com.br/listar-pf/">Clientes PF</a></li> -->
-                    <li><a href="http://vacinarte-admin.com.br/listar-pj/">Pessoa jurídica</a></li>
-                    <li><a href="http://vacinarte-admin.com.br/listar-campanhas/">Campanhas</a></li>
-                  </ul>
-                </li>
-              </ul>
+                <ul class="nav navbar-nav" style="margin-left: 48vw;">
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle fontMenu" data-toggle="dropdown" 
+                      role="button" aria-haspopup="true" 
+                      aria-expanded="false">Cadastrar <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <!-- <li><a href="http://vacinarte-admin.com.br/cadastrar-pf/">Pessoa física</a></li> -->
+                      <li><a href="http://vacinarte-admin.com.br/cadastrar-pj/">Pessoa jurídica</a></li>
+                      <li><a href="http://vacinarte-admin.com.br/campanha/">Campanha</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                
+                <ul class="nav navbar-nav">
+                  <li class="dropdown">
+                    <a href="#" style="text-decoration: none;" class="dropdown-toggle fontMenu" 
+                      data-toggle="dropdown" role="button" aria-haspopup="true" 
+                      aria-expanded="false">Listar <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <!-- <li><a href="http://vacinarte-admin.com.br/listar-pf/">Clientes PF</a></li> -->
+                      <li><a href="http://vacinarte-admin.com.br/listar-pj/">Pessoa jurídica</a></li>
+                      <li><a href="http://vacinarte-admin.com.br/listar-campanhas/">Campanhas</a></li>
+                    </ul>
+                  </li>
+                </ul>
 
-              <ul class="nav navbar-nav">
-                <!-- <li><a style="text-decoration: none;" href="#" data-toggle="modal" data-target="#modalBtnCad">Cadastrar</a></li> -->
-                <li><a style="text-decoration: none;" class="fontMenu" href="http://vacinarte-admin.com.br/listar-agendamento/">Agenda</a></li>
-                <li><a style="text-decoration: none;" class="fontMenu" href="https://www.vacinarte.com.br/">Site Vacinarte</a></li>
-                <li class="page_item page-item-13 fontMenu"><a style="text-decoration: none;" href="http://vacinarte-admin.com.br/?sair=true">Sair</a></li>
-              </ul>            
-            </div><!-- /.navbar-collapse -->
-          </div><!-- /.container-fluid -->
-        </nav>
-      </div>
-
-  </div>
-
-<div class="container"><!-- container principal-->
-    
-    <div class="row">
-        <div class="col-lg-12">
-          <h3 class="page-header texto_cabeca">Cadastro de Cliente PJ</h3>
+                <ul class="nav navbar-nav">
+                  <!-- <li><a style="text-decoration: none;" href="#" data-toggle="modal" data-target="#modalBtnCad">Cadastrar</a></li> -->
+                  <li><a style="text-decoration: none;" class="fontMenu" href="http://vacinarte-admin.com.br/listar-agendamento/">Agenda</a></li>
+                  <li><a style="text-decoration: none;" class="fontMenu" href="https://www.vacinarte.com.br/">Site Vacinarte</a></li>
+                  <li class="page_item page-item-13 fontMenu"><a style="text-decoration: none;" href="http://vacinarte-admin.com.br/?sair=true">Sair</a></li>
+                </ul>            
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+          </nav>
         </div>
-    </div><!-- fecha div row -->
 
-    <center><span class="help-block"><h4><?php echo $msg_err; ?></h4></span></center>
-    
+    </div>
 
-    <div class="row txtbox"><!-- row formulario -->
-      <form action="#" method="post" style="margin-top: -2vw;">
-        <div class="col-lg-12 col-xs-8">
-            
-            <div class="row">  
-              <div class="form-group col-xs-4 col-xs-offset-3">
-                <label style="font-size: 14px;">Razão Social*</label>
-                <input type="text" name="razao" class="form-control" placeholder="Razão Social da empresa" maxlength="150" <?php echo "value='$cliente->nm_rz_soc'"; ?>/>
-              </div>
-            </div>
-            
-            <div class="row">
-              <div class="form-group col-xs-4 col-xs-offset-3">
-                <label style="font-size: 14px;">Nome fantasia*</label>
-                <input type="text" name="nm_fant" class="form-control" placeholder="Nome fantasia" maxlength="150" <?php echo "value='$cliente->nm_fant'"; ?>/>
-              </div>
-            </div>
-            
-            <div class="row">
-              <div class="form-group col-xs-4 col-xs-offset-3">
-                <label style="font-size: 14px;">CNPJ*</label>
-                <input type="text" id="cnpj" name="cnpj" class="form-control" placeholder="Sem pontos/hífen" <?php echo "value='$cliente->cpf_cnpj'"; ?>/>
-              </div>
-            </div>
-
-        </div>
-        
-        <div class="col-lg-12 col-xs-8" style="margin-top: 1vw;">
-          <div class="row btns">
-            <div class="col-xs-1 col-xs-offset-3">
-              <input id="btn_salvar" type="submit" class="button btn btn-danger btn_geral" value="Salvar"/>
-            </div>
-            <div class="col-xs-1" style="margin-left: 2vw;">
-
-              <input id="btn_end" type="button" class="btn btn_geral btn_endereco" onclick="location.href='http://vacinarte-admin.com.br/listar-enderecos/?id=<?php echo $id_retorno; ?>';" 
-              value="Endereços" <?php if ($id_retorno <=0) { echo "disabled='true' style='background-color:slateGray'"; } ?>/>
-            </div> 
-            <div class="col-xs-1" style="margin-left: 2vw;">
-              <input id="btn_ctt" type="button" class="btn btn_geral btn_contato" onclick="location.href='http://vacinarte-admin.com.br/listar-contatos/?id=<?php echo $id_retorno; ?>';" 
-              value="Contatos" <?php if ($id_retorno <=0) { echo "disabled='true' style='background-color:slateGray'"; } ?>/>
-            </div> 
+    <div class="container"><!-- container principal-->
+      
+      <div class="row">
+          <div class="col-lg-12">
+            <h3 class="page-header texto_cabeca">Cadastro de Cliente PJ</h3>
           </div>
-        </div><!-- fecha col 12 -->
-      </form><!-- fecha form -->
-    </div><!-- fecha row txtbox -->
+      </div><!-- fecha div row -->
 
-    
+      <center><span class="help-block"><h4><?php echo $msg_err; ?></h4></span></center>
+      
 
+      <div class="row txtbox"><!-- row formulario -->
+        <form action="#" method="post" style="margin-top: -2vw;">
+          <div class="col-lg-12 col-xs-8">
+              
+              <div class="row">  
+                <div class="form-group col-xs-4 col-xs-offset-3">
+                  <label style="font-size: 14px;">Razão Social*</label>
+                  <input type="text" name="razao" class="form-control" placeholder="Razão Social da empresa" maxlength="150" <?php echo "value='$cliente->nm_rz_soc'"; ?>/>
+                </div>
+              </div>
+              
+              <div class="row">
+                <div class="form-group col-xs-4 col-xs-offset-3">
+                  <label style="font-size: 14px;">Nome fantasia*</label>
+                  <input type="text" name="nm_fant" class="form-control" placeholder="Nome fantasia" maxlength="150" <?php echo "value='$cliente->nm_fant'"; ?>/>
+                </div>
+              </div>
+              
+              <div class="row">
+                <div class="form-group col-xs-4 col-xs-offset-3">
+                  <label style="font-size: 14px;">CNPJ*</label>
+                  <input type="text" id="cnpj" name="cnpj" class="form-control" placeholder="Sem pontos/hífen" <?php echo "value='$cliente->cpf_cnpj'"; ?>/>
+                </div>
+              </div>
 
-    
-</div><!-- fecha container principal -->  
+          </div>
+          
+          <div class="col-lg-12 col-xs-8" style="margin-top: 1vw;">
+            <div class="row btns">
+              <div class="col-xs-1 col-xs-offset-3">
+                <input id="btn_salvar" type="submit" class="button btn btn-danger btn_geral" value="Salvar"/>
+              </div>
+              <div class="col-xs-1" style="margin-left: 2vw;">
+
+                <input id="btn_end" type="button" class="btn btn-danger btn_geral btn_endereco" onclick="location.href='http://vacinarte-admin.com.br/listar-enderecos/?id=<?php echo $id_retorno; ?>';" 
+                value="Endereços" <?php if ($id_retorno <=0) { echo "disabled='true' style='background-color:slateGray'"; } ?>/>
+              </div> 
+              <div class="col-xs-1" style="margin-left: 2vw;">
+                <input id="btn_ctt" type="button" class="btn btn-danger btn_geral btn_contato" onclick="location.href='http://vacinarte-admin.com.br/listar-contatos/?id=<?php echo $id_retorno; ?>';" 
+                value="Contatos" <?php if ($id_retorno <=0) { echo "disabled='true' style='background-color:slateGray'"; } ?>/>
+              </div> 
+            </div>
+          </div><!-- fecha col 12 -->
+        </form><!-- fecha form -->
+      </div><!-- fecha row txtbox -->
+      
+    </div><!-- fecha container principal -->  
 
     <script src="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/jquery.min.js"></script>
     <script src="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/bootstrap.min.js"></script>
