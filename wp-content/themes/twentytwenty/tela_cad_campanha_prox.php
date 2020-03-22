ce502a6488f1d392ec34995997ebb86fdee142fe
ReiDaltro <rei.dev.syst@gmail.com>
2020-03-22 01:43:17 -0300

    testando menu direto na tela cad camp3

--- wp-content/themes/twentytwenty/tela_cad_campanha_prox.php
+++ wp-content/themes/twentytwenty/tela_cad_campanha_prox.php
@@ -109,8 +109,6 @@ if($acao == 'salvar'){
     <!--  Styles -->
     <link href="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/css/styles.css" rel="stylesheet" >
 
-    <script type='text/javascript' src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-
   </head>
   <style>
   .help-block{
@@ -124,13 +122,14 @@ if($acao == 'salvar'){
   }
   .texto_cabeca{
     font-size: 25px;
-    margin-top: 2.5vw !important;
+    margin-top: 1vw !important;
     color: dimgray;
   }
   #btn_salvar, #btn_vac{
     width: 8vw;
     font-size: 14px;
     border-radius: 6px;
+    height: 3vw;
   }
   #btn_vac{
     padding-left: 18px;
@@ -139,114 +138,182 @@ if($acao == 'salvar'){
   .formCadCmp{
     margin-top: -2vw;
   }
+  .barra4vw{
+    height: 4vw !important;
+  }
+  .cabeca{
+    border: none;
+    margin-left: -15px;
+    width: 103%;
+  }
+  .link_home{
+    margin-left: 2vw;
+    text-decoration: none;
+    color: #353b48;
+    font-size: 18px;
+    font-weight: bold;
+  }
+  .fontMenu{
+    font-size: 15px;
+    font-weight: bold;
+  }
   </style>
   <body class="corpo">
-  <?php include 'tela_header.php';?>
-
-  <?php if ($_COOKIE["logado"] <= 0){
+  
+    <?php if ($_COOKIE["logado"] <= 0){
         echo "<script language='javascript' type='text/javascript'>
         window.location.href='http://vacinarte-admin.com.br/';</script>";
     }?>
-<div class="container"><!-- container principal-->
-    
-    <div class="row formCadCmp">
-        <div class="col-lg-12 col-xs-12">
-          <h3 class="page-header texto_cabeca">Cadastro de Campanha</h3>
+
+    <div class="container-fluid barra4vw">
+      <!-- <span><a class="" data-toggle="modal" data-target="#modalBtnCad">Cadastrar</a></span> -->
+      <div >
+          <nav class="navbar navbar-default cabeca barra4vw">
+            <div class="container-fluid barra4vw" style="background-color: Gainsboro;">
+              <!-- Brand and toggle get grouped for better mobile display -->
+          
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="navbar barra4vw">
+                <ul class="nav navbar-nav" style="margin-top: 1vw; float: left;">
+                  <a class="link_home" href="http://vacinarte-admin.com.br/home"><span>Vacinarte</span></a>
+                </ul>
+
+                <ul class="nav navbar-nav" style="margin-left: 48vw;">
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle fontMenu" data-toggle="dropdown" 
+                      role="button" aria-haspopup="true" 
+                      aria-expanded="false">Cadastrar <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <!-- <li><a href="http://vacinarte-admin.com.br/cadastrar-pf/">Pessoa física</a></li> -->
+                      <li><a href="http://vacinarte-admin.com.br/cadastrar-pj/">Pessoa jurídica</a></li>
+                      <li><a href="http://vacinarte-admin.com.br/campanha/">Campanha</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                
+                <ul class="nav navbar-nav">
+                  <li class="dropdown">
+                    <a href="#" style="text-decoration: none;" class="dropdown-toggle fontMenu" 
+                      data-toggle="dropdown" role="button" aria-haspopup="true" 
+                      aria-expanded="false">Listar <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <!-- <li><a href="http://vacinarte-admin.com.br/listar-pf/">Clientes PF</a></li> -->
+                      <li><a href="http://vacinarte-admin.com.br/listar-pj/">Pessoa jurídica</a></li>
+                      <li><a href="http://vacinarte-admin.com.br/listar-campanhas/">Campanhas</a></li>
+                    </ul>
+                  </li>
+                </ul>
+
+                <ul class="nav navbar-nav">
+                  <!-- <li><a style="text-decoration: none;" href="#" data-toggle="modal" data-target="#modalBtnCad">Cadastrar</a></li> -->
+                  <li><a style="text-decoration: none;" class="fontMenu" href="http://vacinarte-admin.com.br/listar-agendamento/">Agenda</a></li>
+                  <li><a style="text-decoration: none;" class="fontMenu" href="https://www.vacinarte.com.br/">Site Vacinarte</a></li>
+                  <li class="page_item page-item-13 fontMenu"><a style="text-decoration: none;" href="http://vacinarte-admin.com.br/?sair=true">Sair</a></li>
+                </ul>            
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+          </nav>
         </div>
-    </div><!-- fecha div row -->
 
-    <center><span class="help-block"><h4><?php echo $msg_err; ?></h4></span></center>
+    </div>
 
-    <div class="row formCadCmp"><!-- row formulario -->
-      <div class="col-lg-12 col-xs-12">
-        <form class="form" action="#" method="post">
-          <div class="hide">
-            <input type="text" id="acao" name="acao" class="form-control"
-                value="salvar"/>
-            <input type="text" id="cd_cli" name="cd_cli" class="form-control"
-                value="<?php echo $cd_cli; ?>"/>
-            <input type="text" id="tp_srv" name="tp_srv" class="form-control"
-              value="<?php echo $tp_srv; ?>"/>
-            <input type="text" id="local_srv" name="local_srv" class="form-control"
-              value="<?php echo $local_srv; ?>"/>
+    <div class="container"><!-- container principal-->
+      
+      <div class="row formCadCmp">
+          <div class="col-lg-12 col-xs-12">
+            <h3 class="page-header texto_cabeca">Cadastro de Campanha</h3>
           </div>
-          <div class="row">  
-            <div class="form-group col-xs-4 col-xs-offset-3">
-              <label style="font-size: 14px;">Campanha</label>
-              <input type="text" id="campanha" name="campanha" class="form-control"
-              value="<?php echo $campanha; ?>"/>
+      </div><!-- fecha div row -->
+
+      <center><span class="help-block"><h4><?php echo $msg_err; ?></h4></span></center>
+
+      <div class="row formCadCmp"><!-- row formulario -->
+        <div class="col-lg-12 col-xs-12">
+          <form class="form" action="#" method="post">
+            <div class="hide">
+              <input type="text" id="acao" name="acao" class="form-control"
+                  value="salvar"/>
+              <input type="text" id="cd_cli" name="cd_cli" class="form-control"
+                  value="<?php echo $cd_cli; ?>"/>
+              <input type="text" id="tp_srv" name="tp_srv" class="form-control"
+                value="<?php echo $tp_srv; ?>"/>
+              <input type="text" id="local_srv" name="local_srv" class="form-control"
+                value="<?php echo $local_srv; ?>"/>
             </div>
-          </div>
-          <div class="row">
-            <div class="form-group col-xs-4 col-xs-offset-3">
-              <label style="font-size: 14px;">Endereço</label>
-              <select class="selectpicker form-control" id="cd_vcl_end" name="cd_vcl_end">
-              <option value=""></option>
-              <?php
-                $enderecos = $wpdb->get_results( 
-                  "
-                  SELECT 
-                    `VCL_ENDERECO`.`cd_cli`,
-                      `VCL_ENDERECO`.`cd_vcl_end`,
-                      `ENDERECO`.`cd_end`, 
-                      `nm_end`, 
-                      `logradouro`, 
-                      `num_end`, 
-                      `complemento`, 
-                      `bairro`, 
-                      `cep`, 
-                      `cidade`, 
-                      `estado`
-                  FROM `ENDERECO`, `VCL_ENDERECO` 
-                  WHERE `ENDERECO`.`cd_end`=`VCL_ENDERECO`.`cd_end` and 
-                  `VCL_ENDERECO`.`cd_cli`={$cd_cli}
-                  "
-                );
-
-                foreach ( $enderecos as $endereco ) 
-                {
-              ?>
-                  <option value='<?php echo $endereco->cd_vcl_end; ?>'><?php echo $endereco->nm_end . ": " . $endereco->logradouro . ", " . $endereco->num_end . " - " . $endereco->bairro; ?></option>
-              <?php
-                }
-              ?>
-              </select>
-              
+            <div class="row">  
+              <div class="form-group col-xs-4 col-xs-offset-3">
+                <label style="font-size: 14px;">Campanha</label>
+                <input type="text" id="campanha" name="campanha" class="form-control"
+                value="<?php echo $campanha; ?>"/>
+              </div>
             </div>
-          </div>
+            <div class="row">
+              <div class="form-group col-xs-4 col-xs-offset-3">
+                <label style="font-size: 14px;">Endereço</label>
+                <select class="selectpicker form-control" id="cd_vcl_end" name="cd_vcl_end">
+                <option value=""></option>
+                <?php
+                  $enderecos = $wpdb->get_results( 
+                    "
+                    SELECT 
+                      `VCL_ENDERECO`.`cd_cli`,
+                        `VCL_ENDERECO`.`cd_vcl_end`,
+                        `ENDERECO`.`cd_end`, 
+                        `nm_end`, 
+                        `logradouro`, 
+                        `num_end`, 
+                        `complemento`, 
+                        `bairro`, 
+                        `cep`, 
+                        `cidade`, 
+                        `estado`
+                    FROM `ENDERECO`, `VCL_ENDERECO` 
+                    WHERE `ENDERECO`.`cd_end`=`VCL_ENDERECO`.`cd_end` and 
+                    `VCL_ENDERECO`.`cd_cli`={$cd_cli}
+                    "
+                  );
 
-          <div class="row">
-            <div class="form-group col-xs-2 col-xs-offset-3">
-              <label style="font-size: 14px;">Data de início</label>
-              <input type="text" id="dt_ini" name="dt_ini" class="form-control"
-              value="<?php echo $dt_ini; ?>"/>
+                  foreach ( $enderecos as $endereco ) 
+                  {
+                ?>
+                    <option value='<?php echo $endereco->cd_vcl_end; ?>'><?php echo $endereco->nm_end . ": " . $endereco->logradouro . ", " . $endereco->num_end . " - " . $endereco->bairro; ?></option>
+                <?php
+                  }
+                ?>
+                </select>
+                
+              </div>
             </div>
 
-            <div class="form-group col-xs-2">
-              <label style="font-size: 14px;">Data de término</label>
-              <input type="text" id="dt_fim" name="dt_fim" class="form-control"
-              value="<?php echo $dt_fim; ?>"/>
+            <div class="row">
+              <div class="form-group col-xs-2 col-xs-offset-3">
+                <label style="font-size: 14px;">Data de início</label>
+                <input type="text" id="dt_ini" name="dt_ini" class="form-control"
+                value="<?php echo $dt_ini; ?>"/>
+              </div>
+
+              <div class="form-group col-xs-2">
+                <label style="font-size: 14px;">Data de término</label>
+                <input type="text" id="dt_fim" name="dt_fim" class="form-control"
+                value="<?php echo $dt_fim; ?>"/>
+              </div>
             </div>
-          </div>
 
-          <div class="row btns" style="margin-top: 1vw;">
-            <div class="col-xs-1 col-xs-offset-3">
-              <input id="btn_salvar" type="submit" class="button btn btn-danger btn_salvar" value="Salvar">
+            <div class="row btns" style="margin-top: 1vw;">
+              <div class="col-xs-1 col-xs-offset-3">
+                <input id="btn_salvar" type="submit" class="button btn btn-danger btn_salvar" value="Salvar">
+              </div>
+              <div class="col-xs-1">
+                <input id="btn_vac" type="button" onclick="location.href='http://vacinarte-admin.com.br/cadastrar-vacina-campanha/?id=<?php echo $id_cmp; ?>';" 
+                value="Vacinas" <?php if ($id_cmp <= 0) { echo "disabled='true' style='background-color:slateGray'"; } ?>/>
+              </div>  
             </div>
-            <div class="col-xs-1">
-              <input id="btn_vac" type="button" onclick="location.href='http://vacinarte-admin.com.br/cadastrar-vacina-campanha/?id=<?php echo $id_cmp; ?>';" 
-              value="Vacinas" <?php if ($id_cmp <= 0) { echo "disabled='true' style='background-color:slateGray'"; } ?>/>
-            </div>  
-          </div>
 
-        </form><!-- fecha form -->
-      </div><!-- fecha col 12 -->
-    </div><!-- fecha row txtbox -->
-
-
-
-    
-</div><!-- fecha container principal -->  
+          </form><!-- fecha form -->
+        </div><!-- fecha col 12 -->
+      </div><!-- fecha row txtbox -->
+  
+    </div><!-- fecha container principal -->  
 
 	  <script src="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/jquery.min.js"></script>
     <script src="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/bootstrap.min.js"></script>
