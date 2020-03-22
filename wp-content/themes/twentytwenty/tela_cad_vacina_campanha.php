e14b011062fff36f7169fd37d57ec52f009dad54
ReiDaltro <rei.dev.syst@gmail.com>
2020-03-22 17:39:55 -0300

    ultimos ajusts de css 7

--- wp-content/themes/twentytwenty/tela_cad_vacina_campanha.php
+++ wp-content/themes/twentytwenty/tela_cad_vacina_campanha.php
@@ -33,9 +33,6 @@ function form_valido() {
         $valido = true;
   }
   
-  echo "<script language='javascript' type='text/javascript'>
-      alert({$vlr_vcna});</script>";
-
   return $valido;
 }
 
