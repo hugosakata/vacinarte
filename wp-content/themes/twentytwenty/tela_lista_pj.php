e0e17e4a889ad94f1a99d8957f9f5863f5652fe6
hugo <hugo_3wl@yahoo.com.br>
2020-04-03 19:48:02 -0300

    teste voltar

--- wp-content/themes/twentytwenty/tela_lista_pj.php
+++ wp-content/themes/twentytwenty/tela_lista_pj.php
@@ -22,9 +22,11 @@ $home = get_home_url();
     <meta charset="utf-8">
     <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="no-cache">
-    <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
-    <META HTTP-EQUIV="Expires" CONTENT="-1">
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
    
     <title>Clientes PJ</title>
     <!-- Bootstrap -->
