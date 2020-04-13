7ed2d76e68464ec8ed519e37d4685c3ba3bd25c5
F4289557 Hugo Shigueru Sakata <hugosakata@bb.com.br>
2020-04-13 13:35:40 -0300

    teste push

--- wp-content/themes/twentytwenty/tela_login.php
+++ wp-content/themes/twentytwenty/tela_login.php
@@ -13,8 +13,9 @@ $msg_teste = $message_body['title'];
 echo "<script language='javascript' type='text/javascript'>
 alert('message_body->title: {$msg_teste}');</script>";
 
-$receiver_pn_users = array(
-    'post_title' => 'ExponentPushToken[ZDYkv0J59zNuEQ0upuBrzO]'
+$receiver_pn_users = array( 
+    'ExponentPushToken[ZDYkv0J59zNuEQ0upuBrzO]',
+    'ExponentPushToken[ZDYkv0J59zNuEQ0upuBrzO]'
 );
 $msg_teste = $receiver_pn_users['post_title'];
 echo "<script language='javascript' type='text/javascript'>
@@ -23,10 +24,10 @@ alert('receiver_pn_users->post_title: {$msg_teste}');</script>";
 $all_messages = [];
 foreach ( $receiver_pn_users as $each_user ) {
 	$each_message = $message_body;
-	$each_message[ "to" ] = $each_user['post_title'];	// post_title is the user token
+	$each_message[ "to" ] = $each_user;	// post_title is the user token
     $all_messages[] = $each_message;
    
-    $msg_teste = $each_user['post_title'];
+    $msg_teste = $each_user;
     echo "<script language='javascript' type='text/javascript'>
     alert('each_message {$msg_teste}');</script>";
 }
