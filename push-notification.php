7a257c9cc9311566c58a58be5b276e2d2ffbd2a3
F4289557 Hugo Shigueru Sakata <hugosakata@bb.com.br>
2020-04-13 13:49:22 -0300

    teste push

--- push-notification.php
+++ push-notification.php
@@ -1,23 +1,39 @@
 <?php
-$message_body = [
-	"title" => "teste push notification",
-	"body" => "teste body teste",
-];
 
-$receiver_pn_users = [
-    "post_title" => "ExponentPushToken[ZDYkv0J59zNuEQ0upuBrzO]"
-];
+$message_body = array(
+	'title' => 'teste push notification cron',
+	'body' => 'teste pelo cron teste'
+);
+// $msg_teste = $message_body['title'];
+// echo "<script language='javascript' type='text/javascript'>
+// alert('message_body->title: {$msg_teste}');</script>";
+
+$receiver_pn_users = array( 
+    'ExponentPushToken[ZDYkv0J59zNuEQ0upuBrzO]'
+);
+// $msg_teste = $receiver_pn_users;
+// echo "<script language='javascript' type='text/javascript'>
+// alert('receiver_pn_users->post_title: {$msg_teste}');</script>";
 
 $all_messages = [];
 foreach ( $receiver_pn_users as $each_user ) {
 	$each_message = $message_body;
-	$each_message[ "to" ] = $each_user->post_title;	// post_title is the user token
-	$all_messages[] = $each_message;
+	$each_message[ "to" ] = $each_user;	// post_title is the user token
+    $all_messages[] = $each_message;
+   
+    // $msg_teste = $each_user;
+    // echo "<script language='javascript' type='text/javascript'>
+    // alert('each_message {$msg_teste}');</script>";
 }
 
 $all_messages_chucked = array_chunk( $all_messages, 99 );
 $responses = [];
 foreach ( $all_messages_chucked as $each_messages_chucked ) {
+
+    // $msg_teste = $each_messages_chucked['to'];
+    // echo "<script language='javascript' type='text/javascript'>
+    // alert('enviando push {$msg_teste}');</script>";
+
 	// Ref: https://docs.expo.io/versions/latest/guides/push-notifications/#http2-api
 	$responses[] = wp_safe_remote_post( "https://exp.host/--/api/v2/push/send", [
 		'method' => 'POST',
@@ -27,3 +43,7 @@ foreach ( $all_messages_chucked as $each_messages_chucked ) {
 		'body' => json_encode( $each_messages_chucked ),
 	] );
 }
+
+// echo "<script language='javascript' type='text/javascript'>
+// alert('push enviado');</script>";
+return "push enviado";
--- wp-content/themes/twentytwenty/tela_login.php
+++ wp-content/themes/twentytwenty/tela_login.php
@@ -3,55 +3,6 @@
 //mudar as variaveis no arquiv wp-config.php
 //retirar o site home e url
 //comentar o banco de desenv e colocaro banco producao
-
-
-$message_body = array(
-	'title' => 'teste push notification',
-	'body' => 'teste 1343 teste'
-);
-$msg_teste = $message_body['title'];
-echo "<script language='javascript' type='text/javascript'>
-alert('message_body->title: {$msg_teste}');</script>";
-
-$receiver_pn_users = array( 
-    'ExponentPushToken[ZDYkv0J59zNuEQ0upuBrzO]'
-);
-$msg_teste = $receiver_pn_users;
-echo "<script language='javascript' type='text/javascript'>
-alert('receiver_pn_users->post_title: {$msg_teste}');</script>";
-
-$all_messages = [];
-foreach ( $receiver_pn_users as $each_user ) {
-	$each_message = $message_body;
-	$each_message[ "to" ] = $each_user;	// post_title is the user token
-    $all_messages[] = $each_message;
-   
-    $msg_teste = $each_user;
-    echo "<script language='javascript' type='text/javascript'>
-    alert('each_message {$msg_teste}');</script>";
-}
-
-$all_messages_chucked = array_chunk( $all_messages, 99 );
-$responses = [];
-foreach ( $all_messages_chucked as $each_messages_chucked ) {
-
-    $msg_teste = $each_messages_chucked['to'];
-    echo "<script language='javascript' type='text/javascript'>
-    alert('enviando push {$msg_teste}');</script>";
-
-	// Ref: https://docs.expo.io/versions/latest/guides/push-notifications/#http2-api
-	$responses[] = wp_safe_remote_post( "https://exp.host/--/api/v2/push/send", [
-		'method' => 'POST',
-		'timeout' => 15,
-		'httpversion' => '2.0',
-		'headers' => [ "content-type" => "application/json" ],
-		'body' => json_encode( $each_messages_chucked ),
-	] );
-}
-
-echo "<script language='javascript' type='text/javascript'>
-alert('push enviado');</script>";
-
 global $wpdb;
 
 $home = get_home_url();
