d2326520715d3d4e3899aa80b603f8ab4ab5fae0
hugo <hugo_3wl@yahoo.com.br>
2020-04-19 12:47:39 -0300

    alteracao cadastro campanha

--- wp-content/themes/twentytwenty/tela_cad_campanha.php
+++ wp-content/themes/twentytwenty/tela_cad_campanha.php
@@ -306,7 +306,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input id="btn_end" type="button" class="btn btn-danger btn_geral btn_endereco" onclick="location.href='<?php echo $home; ?>/listar-enderecos/?id=<?php echo $id_retorno; ?>';" 
                 value="Endereços" <?php if ($id_retorno <=0) { echo "disabled='true' style='background-color:slateGray'"; } ?>/>
               </div> 
-              <div class="col-xs-1" style="margin-left: 2vw;">
+              <div class="col-xs-1">
                 <input id="btn_ctt" type="button" class="btn btn-danger btn_geral btn_contato" onclick="location.href='<?php echo $home; ?>/listar-contatos/?id=<?php echo $id_retorno; ?>';" 
                 value="Contatos" <?php if ($id_retorno <=0) { echo "disabled='true' style='background-color:slateGray'"; } ?>/>
               </div> 
