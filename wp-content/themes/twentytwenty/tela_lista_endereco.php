3fa89364416caef68524fd77fafcc0f64ae3e4e2
hugo <hugo_3wl@yahoo.com.br>
2020-04-21 19:39:29 -0300

    teste excluir endereco campanha

--- wp-content/themes/twentytwenty/tela_lista_endereco.php
+++ wp-content/themes/twentytwenty/tela_lista_endereco.php
@@ -27,12 +27,14 @@ if ($id_cli > 0) {
   $novo = $home.'/cadastrar-endereco-campanha/?id_cmp='.$id_cmp; 
   $sql = "
         SELECT 
-        `ENDERECO`.cd_end, `nm_end`, `logradouro`, `num_end`, `bairro`, `cep`, `cidade`, `estado`, `ativo` 
+        `ENDERECO`.cd_end, `nm_end`, `logradouro`, `num_end`, `bairro`, `cep`, `cidade`, `estado`, `VCL_END_CMP`.`ativo` 
         FROM `ENDERECO`,
         `VCL_END_CMP`
         WHERE 
         `ENDERECO`.cd_end=`VCL_END_CMP`.cd_end and 
-        `VCL_END_CMP`.cd_cmp={$id_cmp} and `VCL_END_CMP`.ativo=1 and `ENDERECO`.ativo=1 order by `nm_end`, `logradouro`
+        `VCL_END_CMP`.cd_cmp={$id_cmp} and 
+        `VCL_END_CMP`.ativo=1 and 
+        `ENDERECO`.ativo=1 order by `nm_end`, `logradouro`
         ";
 }
 
