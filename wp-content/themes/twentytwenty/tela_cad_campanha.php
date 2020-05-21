c555586681cc184cf062b9b3ae0a529b36994ec4
hugo <hugo_3wl@yahoo.com.br>
2020-05-21 20:14:12 -0300

    retirando alerta

--- wp-content/themes/twentytwenty/tela_cad_campanha.php
+++ wp-content/themes/twentytwenty/tela_cad_campanha.php
@@ -38,8 +38,8 @@ function form_valido() {
   $dt_ini = date_converter($data_ini);
   $dt_fim = date_converter($data_fim);
 
-  echo "<script language='javascript' type='text/javascript'>
-    alert('{$nm_cmp}, {$cd_cli}, {$tp_srv}, {$local_srv}, {$dt_ini}, {$dt_fim}');</script>";
+  // echo "<script language='javascript' type='text/javascript'>
+  //   alert('{$nm_cmp}, {$cd_cli}, {$tp_srv}, {$local_srv}, {$dt_ini}, {$dt_fim}');</script>";
 
   $valido = false;
   if (!empty($nm_cmp) &&
