019132ca75a66e22dc9a64042879b84e5c9c9727
hugo <hugo_3wl@yahoo.com.br>
2020-05-21 20:04:51 -0300

    incluindo mascara

--- wp-content/themes/twentytwenty/tela_cad_campanha.php
+++ wp-content/themes/twentytwenty/tela_cad_campanha.php
@@ -323,8 +323,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       
     </div><!-- fecha container principal -->  
 
-	  <script src="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/jquery.min.js"></script>
-    <script src="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/bootstrap.min.js"></script>
+	  <script src="http://www.vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/jquery.min.js"></script>
+    <script src="http://www.vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>
     <script type="text/javascript">
       $(document).ready(function(){	
         $("#dt_ini").mask("99/99/9999");
