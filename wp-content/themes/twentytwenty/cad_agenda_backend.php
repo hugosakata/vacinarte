0195871b2d6291598e070e6cb69da93f43677a26
hugo <hugo_3wl@yahoo.com.br>
2020-06-13 22:13:43 -0300

    teste multiplas vacinas

--- wp-content/themes/twentytwenty/cad_agenda_backend.php
+++ wp-content/themes/twentytwenty/cad_agenda_backend.php
@@ -25,8 +25,9 @@ if(isset($_GET['id'])){
           ";
 
     
-  $campanha = $wpdb->get_results($sql);
-  foreach($campanha as $campanha_item) {
+  $campanha = $wpdb->get_row($sql);
+  $campanhas = $wpdb->get_result($sql);
+  foreach($campanhas as $campanha_item) {
     array_push($ids_vacinas, array("id" => $campanha_item->cd_vcl_vcna_cmp, "valor" => ""));
   }
   echo "<script language='javascript' type='text/javascript'>
