6696f87bfe767ff30a28cfae606b9d333235bac1
ReiDaltro <rei.dev.syst@gmail.com>
2020-07-01 21:16:34 -0300

    correcao query

--- wp-content/themes/twentytwenty/tela_homelogada.php
+++ wp-content/themes/twentytwenty/tela_homelogada.php
@@ -14,10 +14,10 @@ $total_clientes_pf = $wpdb->get_var($sql);
 $sql = "SELECT count(*) as total FROM CLIENTES where cd_tp_cli=2";
 $total_clientes_pj = $wpdb->get_var($sql);
 
-$sql = "SET time_zone = '-3:00'; SELECT COUNT(CD_CMP) QTD_CMP FROM CAMPANHA WHERE DT_FIM >= NOW();";
+$sql = "SET time_zone = '-3:00'; SELECT COUNT(CD_CMP) QTD_CMP FROM CAMPANHA WHERE DT_FIM >= date(NOW());";
 $total_campanha_ativa = $wpdb->get_var($sql);
 
-$sql = "SET time_zone = '-3:00'; SELECT COUNT(CD_ATEND) QT_AGENDA FROM ATENDIMENTO WHERE DT_ATEND >= NOW();";
+$sql = "SET time_zone = '-3:00'; SELECT COUNT(CD_ATEND) QT_AGENDA FROM ATENDIMENTO WHERE DT_ATEND >= date(NOW());";
 $total_agenda = $wpdb->get_var($sql);
 
 ?>
