fe31f4a9ed74b9cdad1fe7c8d1fc262dbdbe4893
hugo <hugo_3wl@yahoo.com.br>
2020-07-04 19:19:04 -0300

    teste upload file

--- wp-content/themes/twentytwenty/tela_cad_file.php
+++ wp-content/themes/twentytwenty/tela_cad_file.php
@@ -9,20 +9,22 @@ $home = get_home_url();
 
 
 <?php
-$uploaddir = '/var/www/uploads/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $uploaddir = '/var/www/uploads/';
+  $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
-echo '<pre>';
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "Arquivo válido e enviado com sucesso.\n";
-} else {
-    echo "Possível ataque de upload de arquivo!\n";
+  echo '<pre>';
+  if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+      echo "Arquivo válido e enviado com sucesso.\n";
+  } else {
+      echo "Possível ataque de upload de arquivo!\n";
+  }
+
+  echo 'Aqui está mais informações de debug:';
+  print_r($_FILES);
+
+  print "</pre>";
 }
-
-echo 'Aqui está mais informações de debug:';
-print_r($_FILES);
-
-print "</pre>";
 
 ?>
 
