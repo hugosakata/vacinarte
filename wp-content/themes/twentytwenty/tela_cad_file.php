25342800667477db5f099b7b157b503da9a69ac7
hugo <hugo_3wl@yahoo.com.br>
2020-07-04 19:15:24 -0300

    teste upload file

---
+++ wp-content/themes/twentytwenty/tela_cad_file.php
@@ -0,0 +1,164 @@
+<?php /* Template Name: CadastroFile */
+//cada vez q o header carregar renova a sessao de logado
+setcookie("logado", 1, (time() + (0.5 * 3600)));
+
+global $wpdb;
+
+$home = get_home_url(); 
+?>
+
+
+<?php
+$uploaddir = '/var/www/uploads/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "Arquivo válido e enviado com sucesso.\n";
+} else {
+    echo "Possível ataque de upload de arquivo!\n";
+}
+
+echo 'Aqui está mais informações de debug:';
+print_r($_FILES);
+
+print "</pre>";
+
+?>
+
+ 
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Clientes - Cadastro</title>
+    <!-- Bootstrap -->
+    <link href="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Cesup Styles -->
+    <link href="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/css/styles.css" rel="stylesheet" >
+
+    <script type="text/javascript" >
+    alert(performance.navigation.type);
+    if(performance.navigation.type == 2){
+      alert("reload");
+      location.reload(true);
+    }
+    </script>
+
+    <script type="text/javascript" >
+    
+    function limpa_formulário_cep() {
+            //Limpa valores do formulário de cep.
+            document.getElementById('logra').value=("");
+            document.getElementById('bairro').value=("");
+            document.getElementById('cidade').value=("");
+            document.getElementById('uf_br').value=("");
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('logra').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf_br').value=(conteudo.uf);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+        
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor.replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('logra').value="...";
+                document.getElementById('bairro').value="...";
+                document.getElementById('cidade').value="...";
+                document.getElementById('uf_br').value="...";
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
+
+    </script>
+
+    <style type="text/css">
+        .help-block{ display: block;
+                     margin-top: 5px;
+                     margin-bottom: 10px;
+                     color: #a94442; }
+    </style>
+
+  </head>
+  <body>
+  <?php include 'tela_header.php';?>
+  <?php if ($_COOKIE["logado"] <= 0){
+        echo "<script language='javascript' type='text/javascript'>
+        window.location.href='http://vacinarte-admin.com.br/';</script>";
+    }?>
+
+<div class="container"><!-- container principal-->
+    
+    <div class="row">
+        <div class="col-lg-12">
+          <h3 class="page-header">Cadastro de Cliente PF
+          <br>
+            <small>Preencha o formulário abaixo para cadastrar um novo cliente</small>
+          </h3>
+        </div>
+    </div><!-- fecha div row -->
+
+    <center><span class="help-block"><h4><?php echo $msg_err; ?></h4></span></center>
+
+<!-- O tipo de encoding de dados, enctype, DEVE ser especificado abaixo -->
+<form enctype="multipart/form-data" action="__URL__" method="POST">
+    <!-- MAX_FILE_SIZE deve preceder o campo input -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+    <!-- O Nome do elemento input determina o nome da array $_FILES -->
+    Enviar esse arquivo: <input name="userfile" type="file" />
+    <input type="submit" value="Enviar arquivo" />
+</form>
+
+</div>  
+
+
+    <script src="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/jquery.min.js"></script>
+    <script src="http://vacinarte-admin.com.br/wp-content/themes/twentytwenty/js/bootstrap.min.js"></script>
+
+  </body>
+  </html>
