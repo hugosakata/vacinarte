9a4480051580e60c38bdeecf91b182e75ed8f2dc
hugo <hugo_3wl@yahoo.com.br>
2020-07-11 14:57:38 -0300

    estrutura do site sql

--- wp-content/themes/twentytwenty/sql/sql_vacinarte.php
+++ wp-content/themes/twentytwenty/sql/sql_vacinarte.php
@@ -1,6 +1,12 @@
 <?php
 
 $lista_campanhas = "
+SELECT *
+FROM CAMPANHA
+WHERE
+cd_cmp = %d";
+
+$lista_campanhassss = "
 SELECT 
 CAMPANHA.`cd_cmp`, `nm_cmp`, `nm_fant`, `cd_vcl_end`, `nm_tp_srv`, date_format(`dt_ini`, '%d/%m/%Y') AS dt_ini, date_format(`dt_fim`, '%d/%m/%Y') AS dt_fim, `VCL_VCNA_CMP`.`cd_vcl_vcna_cmp`, `VACINA`.nm_gen, `VCL_VCNA_CMP`.`qtd_vcna_contratada`, `VCL_VCNA_CMP`.`qtd_vcna_restante`
 FROM `CAMPANHA`, `TP_SRV`, `CLIENTES`, `VCL_VCNA_CMP`, `VACINA`
