8dbb6df63634ea8b6a3f233f3e572d09d78c98db
hugo <hugo_3wl@yahoo.com.br>
2020-07-11 15:03:05 -0300

    estrutura do site sql

--- wp-content/themes/twentytwenty/backend/cad_agenda.php
+++ wp-content/themes/twentytwenty/backend/cad_agenda.php
@@ -5,7 +5,7 @@ $ids_vacinas = array();
 //$wpdb->prepare( 'query' , value_parameter[, value_parameter ... ] );
 if(isset($_GET['id'])){
   $id_cmp = $_GET['id'];//id da campanha
-  $sql = $wpdb->prepare($lista_campanhassss , "%d",  $id_cmp );
+  $sql = $wpdb->prepare($lista_campanhassss , "%d", "%d",  $id_cmp );
 
   echo ">>>".$lista_campanhassss;
   echo ">>>>>>>".$sql;
