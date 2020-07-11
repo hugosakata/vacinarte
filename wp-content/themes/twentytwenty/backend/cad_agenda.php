c28620873b88d98295f8a6c6af06d406b1f8fa9f
hugo <hugo_3wl@yahoo.com.br>
2020-07-11 14:48:22 -0300

    estrutura do site sql

--- wp-content/themes/twentytwenty/backend/cad_agenda.php
+++ wp-content/themes/twentytwenty/backend/cad_agenda.php
@@ -5,7 +5,15 @@ $ids_vacinas = array();
 //$wpdb->prepare( 'query' , value_parameter[, value_parameter ... ] );
 if(isset($_GET['id'])){
   $id_cmp = $_GET['id'];//id da campanha
-  $sql = $wpdb->prepare( $lista_campanhas , $id_cmp );
+  $sql = $wpdb->prepare( "
+  SELECT 
+  CAMPANHA.`cd_cmp`, `nm_cmp`, `nm_fant`, `cd_vcl_end`, `nm_tp_srv`, date_format(`dt_ini`, '%d/%m/%Y') AS dt_ini, date_format(`dt_fim`, '%d/%m/%Y') AS dt_fim, `VCL_VCNA_CMP`.`cd_vcl_vcna_cmp`, `VACINA`.nm_gen, `VCL_VCNA_CMP`.`qtd_vcna_contratada`, `VCL_VCNA_CMP`.`qtd_vcna_restante`
+  FROM `CAMPANHA`, `TP_SRV`, `CLIENTES`, `VCL_VCNA_CMP`, `VACINA`
+  WHERE `CAMPANHA`.`cd_tp_srv`=`TP_SRV`.`cd_tp_srv` AND
+  `CAMPANHA`.`cd_cli`=`CLIENTES`.`cd_cli` and
+  VCL_VCNA_CMP.cd_cmp=CAMPANHA.cd_cmp AND
+  VCL_VCNA_CMP.cd_vcna=VACINA.cd_vcna AND
+  CAMPANHA.cd_cmp = '%d'" , $id_cmp );
 
   $campanhas = $wpdb->get_results($sql);
   $campanha = $campanhas[0];
