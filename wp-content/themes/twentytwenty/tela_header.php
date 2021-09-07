fc3c9386eff946cabb484274e96676aeee1414df
hugo <hugo_3wl@yahoo.com.br>
2021-09-07 01:50:31 -0300

    teste retirar erro

--- wp-content/themes/twentytwenty/tela_header.php
+++ wp-content/themes/twentytwenty/tela_header.php
@@ -3,7 +3,9 @@
 setcookie("logado", 1, (time() + (0.5 * 3600)));
 $home = get_home_url(); 
 
-error_reporting(0);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0)
 ?>
 
 <!DOCTYPE html>
